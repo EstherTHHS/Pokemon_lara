09c9256aa4c5cd57f73c40357d436513b695fd5f
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2024-01-11 10:13:59 +0630

    pokemon

---
+++ database/migrations/2024_01_09_185859_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('price');
+            $table->string('type');
+            $table->string('rarity');
+            $table->boolean('left');
+            $table->string('image_url')->nullable();
+            $table->boolean('status')->default(1)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
